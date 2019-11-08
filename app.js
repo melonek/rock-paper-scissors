504f329dc85d9b2b7b4e5551eaafe455a06621ff
melon <meloniusz@hotmail.com>
2019-11-08 12:45:54 +0800

    fixed error in dom

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("userScore");
-const computerScore_span = document.getElementById("computerScore");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result > p");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -19,13 +19,14 @@ function convertToWord(letter) {
   if (letter === "p") return "Paper";
   return "Scissors";
 }
+
 function win(userChoice, computerChoice) {
   userScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  const smallUserWord = "user".fontsize(3).sup();
-  const smallCompWord = "comp".fontsize(3).sup();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} + beats
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats
     ${convertToWord(computerChoice)}${smallCompWord}. You win! `;
 }
 
@@ -33,16 +34,16 @@ function lose(userChoice, computerChoice) {
   computerScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  const smallUserWord = "user".fontsize(3).sup();
-  const smallCompWord = "comp".fontsize(3).sup();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} + loses to
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to
       ${convertToWord(computerChoice)}${smallCompWord}. You lost...! `;
 }
 
 function draw(userChoice, computerChoice) {
-  const smallUserWord = "user".fontsize(3).sup();
-  const smallCompWord = "comp".fontsize(3).sup();
-  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} + equals
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals
         ${convertToWord(computerChoice)}${smallCompWord}. It's a draw. `;
 }
 
@@ -79,4 +80,5 @@ function main() {
     game("s");
   });
 }
+
 main();
